92deded21b9c42bdfa1b3de7d0a79834ae9ebde3
Mid9658 <54149117+Mid9658@users.noreply.github.com>
2022-11-30 23:02:40 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ class Vch_kankei(commands.Cog, name = "ボイチャ"):
             return
         # 音声を流す準備および音を小さく
         source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("https://kagamiya.work/gallery/voicefile/Q-furi-rugi2.mp3"), volume=0.2)
-        if voice_client.is_connected:
+        if self.voice_client.is_connected:
             voice = guild.voice_client
         else:
             # ぼいちゃに接続する
@@ -89,12 +89,13 @@ class Vch_kankei(commands.Cog, name = "ボイチャ"):
     async def bye_bye(self, ctx: commands.Context):
         """切断するよ"""
         # botがぼいちゃにいないのに切断しようとしたら注意する
-        if not voice_client.is_connected:
+        if not self.voice_client.is_connected:
             await ctx.send("私ぼいちゃにいませんよ。")
             return
 
         # 切断する
         await ctx.message.guild.voice_client.disconnect()
+        VoiceProtocol.cleanup()
         await ctx.send("失礼しました～。")
 
     # かぞえる
